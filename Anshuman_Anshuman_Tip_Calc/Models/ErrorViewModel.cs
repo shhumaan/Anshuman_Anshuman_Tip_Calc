2a982d8ca4bae0dc53845a217ce69e927650ea88
shhumaan <48822497+shhumaan@users.noreply.github.com>
2023-10-26 00:54:06 -0400

    init

---
+++ Anshuman_Anshuman_Tip_Calc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Anshuman_Anshuman_Tip_Calc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
